2057669362c9f7f978222203ecf1ec7694263f6e
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-20 16:28:18 +0500

    Initial commit

---
+++ lib/pages/category_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CategoryPage extends StatelessWidget {
+  const CategoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              // Back Button
+
+              // Text
+
+              // Categories (5x)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              // Card Number
+
+              // CVV & Exp
+
+              // Cardholder Name
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
